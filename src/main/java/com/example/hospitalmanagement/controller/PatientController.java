f8080605ac5b209cad825f62abfb2dc895cf3a44
DoPhuc05 <phuc201005@gmail.com>
2025-10-21 00:07:18 +0700

    Add: tạo PatientRepository và PatientService

--- src/main/java/com/example/hospitalmanagement/controller/PatientController.java
+++ src/main/java/com/example/hospitalmanagement/controller/PatientController.java
@@ -1,5 +1,12 @@
 package com.example.hospitalmanagement.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+@Controller
 public class PatientController {
+    @GetMapping("/patients")
+    public String getPatients() {
+        return "patients"; // trỏ tới templates/patients.html
+    }
 
 }
