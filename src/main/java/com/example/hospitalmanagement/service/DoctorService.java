03aca1728228fad5cc2db0bfcaae80ae741e5b52
DoPhuc05 <phuc201005@gmail.com>
2025-10-21 17:22:18 +0700

    feat: Delete Doctor function in Doctor service

--- src/main/java/com/example/hospitalmanagement/service/DoctorService.java
+++ src/main/java/com/example/hospitalmanagement/service/DoctorService.java
@@ -1,4 +1,14 @@
 package com.example.hospitalmanagement.service;
 
+import org.springframework.stereotype.Service;
+import com.example.hospitalmanagement.repository.DoctorRepository;
+import com.example.hospitalmanagement.model.Doctor;
+import lombok.RequiredArgsConstructor;
+@Service
+@RequiredArgsConstructor
 public class DoctorService {
+    private DoctorRepository doctorRepository;
+    public void deleteDoctor(Long id) {
+        doctorRepository.deleteById(id);
+    }
 }
