9cb2b1b26aa9e2ef4cfdc18a5707b8c239c35f13
DoPhuc05 <phuc201005@gmail.com>
2025-10-21 17:52:00 +0700

    feat: Search doctor by name,specialty in DoctorService

--- src/main/java/com/example/hospitalmanagement/repository/DoctorRepository.java
+++ src/main/java/com/example/hospitalmanagement/repository/DoctorRepository.java
@@ -1,8 +1,14 @@
 package com.example.hospitalmanagement.repository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.hospitalmanagement.model.Doctor;
 
+import java.util.List;
+
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    @Query("SELECT d FROM Doctor d WHERE d.name LIKE %:keyword% OR d.specialty LIKE %:keyword% OR d.email LIKE %:keyword%")
+    List <Doctor> findByKeyword(@Param("keyword") String keyword);
 }
--- src/main/java/com/example/hospitalmanagement/service/DoctorService.java
+++ src/main/java/com/example/hospitalmanagement/service/DoctorService.java
@@ -1,4 +1,20 @@
 package com.example.hospitalmanagement.service;
 
+import org.springframework.stereotype.Service;
+import com.example.hospitalmanagement.repository.DoctorRepository;
+import com.example.hospitalmanagement.model.Doctor;
+
+import java.util.List;
+
+@Service
 public class DoctorService {
+    private final DoctorRepository doctorRepository;
+
+    public DoctorService(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+    public List<Doctor> searchDoctors(String keyword) {
+        return doctorRepository.findByKeyword(keyword);
+    }
 }
