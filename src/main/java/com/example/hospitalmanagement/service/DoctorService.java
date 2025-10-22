50984c6dc1fd06494b82ba8c549c9284b9efb894
igdaniel28 <hphuc281225@gmail.com>
2025-10-22 11:14:55 +0700

    an nham

--- src/main/java/com/example/hospitalmanagement/service/DoctorService.java
+++ src/main/java/com/example/hospitalmanagement/service/DoctorService.java
@@ -16,16 +16,10 @@ public class DoctorService {
 
     private final DoctorRepository doctorRepository;
 
-    /**
-     * Lấy danh sách bác sĩ từ DB (đủ các trường ID, Tên, Chuyên khoa,
-     * Bằng cấp, SĐT, Email, Lịch làm việc vì chúng nằm trong entity Doctor).
-     * Mặc định sắp xếp theo ID tăng dần.
-     */
+
     public List<Doctor> getAllDoctors() {
-        // Cách A: gọi findAll + Sort
+        //  gọi findAll + Sort
         return doctorRepository.findAll(Sort.by(Sort.Direction.ASC, "doctorId"));
 
-        // Cách B: nếu muốn dùng default method trong Repository:
-        // return doctorRepository.findAllOrderByIdAsc();
     }
 }
